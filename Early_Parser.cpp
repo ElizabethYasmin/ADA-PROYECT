728c9bd568a09fe8c32525a11db58ddc9ef4a086
KeepUrName <lucca360@outlook.com>
2021-06-08 07:05:07 -0500

    correccion de estados repetidos en un chart

--- Early_Parser.cpp
+++ Early_Parser.cpp
@@ -58,22 +58,21 @@ private:
     std::vector<produccion>listaDeProducciones;
 };
 
-
 /*
-estado_entrada
+estado
 produccion => produccion
 i => posicion del * de parser
 origen = S[k]
 */
-class estado{
+class tupla{
 private:
     std::string produccion;
-    int i; //posicion del punto de early
+    int i;
     int origen;
     std::string izquierda;
     std::string derecha;
 public: 
-    estado(std::string produccion, int k, int j){
+    tupla(std::string produccion, int k, int j){
         this -> produccion = produccion;
         i = k;
         origen = j;
@@ -104,7 +103,7 @@ public:
     std::string getProduccion(){
         return produccion;
     }
-    //b -> A * Va
+
     /*Devuelve el elemento despues del '*' */
     std::string getNextElement(){ 
         std::string temp;
@@ -156,32 +155,37 @@ public:
         return temp;
     }
 
-    friend ostream& operator<<(ostream& os, const estado& dt);
+    friend ostream& operator<<(ostream& os, const tupla& dt);
 };
 
-ostream& operator<<(ostream& os, const estado& dt){
+ostream& operator<<(ostream& os, const tupla& dt){
     os << dt.izquierda << " => " <<  dt.derecha << " punto: " << dt.i << " origen: " << dt.origen;
     return os;
 }
 
-//verifica que el estado_entrada que se inserte en S no este repetido
-bool estaRepetido(std::vector<estado> S,estado estado_entrada,int k){
+/*
+verifica que el estado que se inserte en S no este repetido
+S => verificar en chart
+estdo => estado averificar
+k => posicon del punto
+*/
+bool estaRepetido(std::vector<tupla> S,tupla estado,int k){
     for (auto it = S.begin(); it != S.end(); it++){
-        estado temp = *it;
-        if(temp.getProduccion() == estado_entrada.getProduccion() && temp.getDotPosition() == k)
+        tupla temp = *it;
+        if(temp.getProduccion() == estado.getProduccion() && temp.getDotPosition() == k)
             return 1;
     }
     return 0;
 }
 
-void predecir(estado temp ,std::vector<estado> S[], int k, std::vector<estado> Grammar){
+void predecir(tupla temp ,std::vector<tupla> S[], int k, std::vector<tupla> Grammar){
    /* form (X → α • Y β, j) , add (Y → • γ, k) */
     std::string Y = temp.getNextElement();
     for (auto it = Grammar.begin(); it != Grammar.end(); it++){
-        estado temp = *it;
+        tupla temp = *it;
         if(temp.getIzquierda()==Y && !estaRepetido(S[k],temp,temp.getDotPosition())){
-                //std::cout<<"++ Anhadiendo "<<temp<<" al estado_entrada "<<k<<std::endl; 
-                S[k].push_back(estado(temp.getProduccion(),temp.getDotPosition(),k));
+                //std::cout<<"++ Anhadiendo "<<temp<<" al estado "<<k<<std::endl; 
+                S[k].push_back(tupla(temp.getProduccion(),temp.getDotPosition(),k));
             
         }
     }
@@ -191,57 +195,55 @@ void predecir(estado temp ,std::vector<estado> S[], int k, std::vector<estado> G
 a => the next symbol in the input stream
 */
 
-/*
-a = number* + number 
-estado_entrada = estado_entrada no terminal = T → • number
-s = charts 
-*/
-void scanneo(std::string a ,std::vector<estado> S[], int k, estado estado_entrada,std::vector<estado> grammar){
-    std::string b = estado_entrada.getNextElement();
+void scanneo(std::string a ,std::vector<tupla> S[], int k, tupla estado,std::vector<tupla> grammar){
+    std::string b = estado.getNextElement();
     std::cout<<"Scanean ..."<<a<<" =? "<<b<<std::endl;
+    std::cout<<" ESTADO: "<<estado<<std::endl;
     // X → α • a β, j , add (X → α a • β, j) to S(k+1) 
 
-    if((a==b) && !estaRepetido(S[k+1],estado_entrada,k+1)){
+    if((a==b) && !estaRepetido(S[k+1],estado,estado.getDotPosition()+1)){
         //caso 1= elemento no terminal sin produccion
-        S[k+1].push_back(estado(estado_entrada.getProduccion(),estado_entrada.getDotPosition()+1,estado_entrada.getOrigen()));
+        std::cout<<"caso 1= elemento no terminal sin produccion"<<std::endl;
+        std::cout<<"+anadiendo: "<<estado.getProduccion()<<" a estado: " <<k+1<<std::endl;
+        S[k+1].push_back(tupla(estado.getProduccion(),estado.getDotPosition()+1,estado.getOrigen()));
     }
     else {
-        //caso 1= elemento no terminal con produccion
-    for (auto it = grammar.begin(); it != grammar.end(); it++){
-        estado temp = *it; 
-        std::cout<<"Elemento: "<<temp.getIzquierda()<<"\n";
-        if(temp.getIzquierda() == b && temp.getDerecha()==a && !estaRepetido(S[k+1],temp,k+1)){
-            S[k+1].push_back(estado(temp.getProduccion(),temp.getDotPosition()+1,k));
+            //caso 2= elemento no terminal con produccion
+            std::cout<<"caso 2= elemento no terminal con produccion"<<std::endl;
+        for (auto it = grammar.begin(); it != grammar.end(); it++){
+            tupla temp = *it; 
+            std::cout<<"Elemento: "<<temp.getIzquierda()<<"\n";
+            std::cout<<"Verificando que: "<<temp<< "origen en: "<<k<<" no este en estado: "<<k+1<<std::endl;
+            if(temp.getIzquierda() == b && temp.getDerecha()==a && !estaRepetido(S[k+1],temp,temp.getDotPosition()+1)){
+                 std::cout<<"+anadiendo: "<<temp<<"origen en: "<<k<<" a estado: " <<k+1<<std::endl;
+                S[k+1].push_back(tupla(temp.getProduccion(),temp.getDotPosition()+1,k));
+            }
         }
     }
-    }
-
-
-
 }
 /*
-character => parte izquierda de la produccion del estado_entrada
+character => parte izquierda de la produccion del estado
 */
-void completar(std::vector<estado> S[],int k, estado estado_entrada){
-    for (auto it2 = S[estado_entrada.getOrigen()].begin(); it2 != S[estado_entrada.getOrigen()].end(); it2++){
-            estado temp2 = *it2;
+void completar(std::vector<tupla> S[],int k, tupla estado){
+    for (auto it2 = S[estado.getOrigen()].begin(); it2 != S[estado.getOrigen()].end(); it2++){
+            tupla temp2 = *it2;
             //std::cout<<"Y: "<<temp.getIzquierda()<<" compare "<<temp2.getNextElement()<<std::endl;
-            if(estado_entrada.getIzquierda() == temp2.getNextElement()  && !estaRepetido(S[k],temp2,temp2.getDotPosition()+1) ){
-                //std::cout<<"++ Anhadiendo "<<temp2<<" al estado_entrada "<<k<<std::endl; 
-                S[k].push_back(estado(temp2.getProduccion(),temp2.getDotPosition()+1,temp2.getOrigen()));
+            if(estado.getIzquierda() == temp2.getNextElement()  && !estaRepetido(S[k],temp2,temp2.getDotPosition()+1) ){
+                //std::cout<<"++ Anhadiendo "<<temp2<<" al estado "<<k<<std::endl; 
+                S[k].push_back(tupla(temp2.getProduccion(),temp2.getDotPosition()+1,temp2.getOrigen()));
             }
         }
 }
 
-bool esterminal(std::vector<estado> G, std::string caracter){
+bool esterminal(std::vector<tupla> G, std::string caracter){
     for (auto it = G.begin(); it != G.end(); it++){
-        estado temp2 = *it;
+        tupla temp2 = *it;
         if(temp2.getIzquierda()==caracter){
             if(temp2.getlength()!=1)
                 return 0;
             else {
                 for (auto it2 = G.begin(); it2 != G.end(); it2++){
-                    estado temp3 = *it2;
+                    tupla temp3 = *it2;
                     if(temp3.getIzquierda()==temp2.getDerecha())
                         return 0;
                 }
@@ -252,7 +254,7 @@ bool esterminal(std::vector<estado> G, std::string caracter){
     return 1;
 }
 
-bool terminado(estado S){
+bool terminado(tupla S){
     //std::cout<<"El punto esta al final? "<<S.getDotPosition()<<" "<<S.getlength()<<std::endl;
     if(S.getDotPosition()>=S.getlength())
         return 1;
@@ -295,42 +297,44 @@ int early_parser(std::string A , std::string Grammar[],int n_proposiciones){
     if (myfile.is_open()){
 
     int tamanoEntrada=tamanoDeEntrada(A);
-    std::vector<estado> grammar;
+    std::vector<tupla> grammar;
     //std::cout<<"Creando gramatica..\n"<<n_proposiciones;
     for(int i=0 ; i<n_proposiciones ; i++){
-        grammar.push_back(estado(Grammar[i],0,0));
+        grammar.push_back(tupla(Grammar[i],0,0));
     }
     //std::cout<<"Creando S(k). .\n";
-    //creacion de estado_entradas
-    std::vector<estado> S[tamanoEntrada+1];
+    //creacion de estados
+    std::vector<tupla> S[tamanoEntrada+1];
     //agrega todos los axiomas a S[0]
     std::string axioma = grammar.at(0).getIzquierda();
     for (auto it = grammar.begin(); it != grammar.end(); it++){
-        estado temp = *it;
+        tupla temp = *it;
         if(axioma==temp.getIzquierda()){
-            S[0].push_back(estado(temp.getProduccion(),0,0));
+            S[0].push_back(tupla(temp.getProduccion(),0,0));
         }
     }
     //recorre el punto en posicion i de la entrada hasta el final 
     for(int i= 0 ; i<tamanoEntrada+1 ; i++){
         myfile<<"========================S: "<<i<<"======================"<<std::endl;
+        std::cout<<"========================S: "<<i<<"======================"<<std::endl;
         for (int j = 0; j<S[i].size() ; j++){
             auto temp = S[i].at(j);
-            myfile<<"estado_entrada:"<<j<<" =>>>> "<<temp<<std::endl;
+            myfile<<"estado:"<<j<<" =>>>> "<<temp<<std::endl;
+            std::cout<<"estado:"<<j<<" =>>>> "<<temp<<std::endl;
             //si el punto de early no esta en el final
             //myfile<<"elemento a analizar: "<<temp.getNextElement();
             if(!terminado(temp)){
                 if(!esterminal(grammar,temp.getNextElement())){
-                    //myfile<<" no es terminal.\n-------Prediciendo ...\n";
+                    std::cout<<" no es terminal.\n-------Prediciendo ...\n";
                     predecir(temp,S,i,grammar);
                 }
                 else{
-                    //myfile<<" es terminal.\n-------Escaneando ...\n";
+                    std::cout<<" es terminal.\n-------Escaneando ...\n";
                     scanneo(elementoNdeEntrada(A,i),S,i,temp,grammar); 
                 }
             }
             else{
-                //myfile<<"\n--------Completando ...\n";
+                std::cout<<"\n--------Completando ...\n";
                 completar(S,i,temp);
             }
         }
@@ -342,13 +346,13 @@ int early_parser(std::string A , std::string Grammar[],int n_proposiciones){
 }
 
 int main(){
-    /*
+   /*
     //Ejemplo Wikipedia
-    std::string A = "number *+ number * number";
+    std::string A = "number + number * number";
     std::string G[] = {
-        "P ::= *S" ,
-        "S ::= S *+ M", 
-        "S ::= *M" ,
+        "P ::= S" ,
+        "S ::= S + M", 
+        "S ::= M" ,
         "M ::= M * T" ,
         "M ::= T" ,
         "T ::= number" ,
@@ -356,7 +360,7 @@ int main(){
     };
     early_parser(A,G,6);
 */
-   /*
+  /* 
     //Ejemplo YT: https://www.youtube.com/watch?v=WNKw1tiskSM&t=1622s
     std::string A = "1 + 1";
     std::string G[] = {
@@ -365,7 +369,7 @@ int main(){
         "number ::= 1" 
     };
     early_parser(A,G,3);
-    */
+  */
    
     //Ejemplo YT: https://www.youtube.com/watch?v=1j6hB3O4hAM&t=462s
     std::string A = "book that flight";
@@ -388,18 +392,14 @@ int main(){
         "verb ::= include" ,
         "verb ::= prefer" ,
     };
-/*
-    Grammar Terminales;
-    Grammar NoTerminales;
-*/
     early_parser(A,G,17);
     
     /*
     int tamanoEntrada=tamanoDeEntrada(A);
-    std::vector<estado> grammar;
+    std::vector<tupla> grammar;
     //std::cout<<"Creando gramatica..\n"<<n_proposiciones;
     for(int i=0 ; i<17 ; i++){
-        grammar.push_back(estado(G[i],0,0));
+        grammar.push_back(tupla(G[i],0,0));
     }
     //std::cout<<getTerminal("NP",grammar).size();
     //std::cout<<esterminal(grammar,"nominal");*/
